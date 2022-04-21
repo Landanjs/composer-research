cd24b616e290e8ca08bb217f0097240d05589f1f
Landan Seguin <landanjs@gmail.com>
2022-04-21 10:43:25 -0700

    Index out of bounds

--- models/deeplabv3.py
+++ models/deeplabv3.py
@@ -238,7 +238,8 @@ class ComposerDeepLabV3(ComposerModel):
         loss = 0
         if self.lambda_dice:
             dice_loss = self.dice_loss(outputs, target.unsqueeze(1))
-            inds, counts = torch.unique(target, return_counts=True)
+            inds, _ = torch.unique(target - 1, return_counts=True)
+            inds = inds[inds != -1]
             loss += dice_loss[inds].mean() * self.lambda_dice
             #loss += dice_loss.pow(1 / self.gamma).mean() * self.lambda_dice
         if self.lambda_focal:
