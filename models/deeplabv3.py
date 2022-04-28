8e4666b473bec838b211566474a308950057b023
Landan Seguin <landanjs@gmail.com>
2022-04-27 18:20:45 -0700

    reverse rebalance

--- models/deeplabv3.py
+++ models/deeplabv3.py
@@ -253,7 +253,7 @@ class ComposerDeepLabV3(ComposerModel):
             #mask = torch.zeros(len(dice_loss), dtype=torch.bool)
             #mask[c_present] = True
             weights = torch.zeros_like(dice_loss)
-            weights[c_present] = counts.float()
+            weights[c_present] = (1. / counts.float())
 
             #weights[mask] = 1
             #weights[~mask] = 0
