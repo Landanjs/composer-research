5faffe8abab3aed7906dce1419455bd04947f6b8
Landan Seguin <landanjs@gmail.com>
2022-05-04 08:47:28 -0700

    Inverse pt 2

--- models/deeplabv3.py
+++ models/deeplabv3.py
@@ -249,7 +249,7 @@ class ComposerDeepLabV3(ComposerModel):
             weights = alpha * (1 / class_count_per_batch.sum(
                 dim=1, keepdim=True)) + (1 - alpha) * (1 / weights)
 
-            class_loss = class_loss / (weights + epsilon)
+            class_loss = class_loss * (weights + epsilon)
             loss = class_loss.sum(dim=1).mean()
 
         return loss
